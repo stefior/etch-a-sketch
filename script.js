a5e28c1a34868526e6d53af5e29ee51fd08867ad
Stefior <stephencfiorenza@gmail.com>
2022-09-08 15:18:14 -0400

    Make new grids flex to same boundary

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const button = document.querySelector('button');
 button.onclick = () => createGrid(Number(prompt("New width: ")), Number(prompt("New height: ")));
 
 function createGrid(width, height) {
-  if (!width || !height) return;
+  if (!width || !height) return alert('incorrect input');
+  
+  const oldGridDivs = document.querySelectorAll('#container > *');
   const containerDiv = document.querySelector('div');
+  oldGridDivs.forEach((oldGridRow) => containerDiv.removeChild(oldGridRow));
+
   for (let i = 0; i < height; i++) {
     const rowDiv = document.createElement('div');
     rowDiv.classList += 'row-div';
@@ -17,4 +21,4 @@ function createGrid(width, height) {
   }
 }
 
-createGrid(16, 16);
+createGrid(8, 8);
